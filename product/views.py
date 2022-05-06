4a43d2fb4d0fccfdc8519d61a1cfb5f6f768ded6
Жанболот Бообеков <joni@MacBook-Air-Zanbolot.local>
2022-05-06 12:10:42 +0600

    settings/media/urls

--- product/views.py
+++ product/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 
 def product_list(request):
-    return render(request, 'product/product_list.html', {'products': 'product test'})
+    products = Product.objects.all()
+    return render(request, 'product/product_list.html', {'products': products})
